719c01798222816f2d16a48bb7e6e0c36d7a5052
sam bacha <sam@freighttrust.com>
2021-03-29 02:54:10 -0700

    fix(user): abstract user into deployer

--- site/src/models/users.js
+++ site/src/models/users.js
@@ -2,6 +2,8 @@ import { signJWT } from '../utils/jwt.js'
 import merge from 'merge-options'
 
 const users = USERS
+// TODO
+// Refactor User into more Abstract 'Deployer'
 
 /**
  * @typedef {{
@@ -16,14 +18,14 @@ const users = USERS
  * @expample
  * ```json
  * {
- *   "nickname":"hugomrdias",
- *   "name":"Hugo Dias",
- *   "picture":"https://avatars.githubusercontent.com/u/314190?v=4",
+ *   "nickname":"SButerin",
+ *   "name":"satoshi buterin",
+ *   "picture":"https://....",
  *   "updated_at":"2021-03-09T20:47:39.307Z",
- *   "email":"hugomrdias@gmail.com",
- *   "email_verified":true,"iss":"https://hugomrdias.eu.auth0.com/",
- *   "sub":"github|314190",
- *   "aud":"HPc0iTCaRYxOSQv5mW9i4Qguzw7mxhLP",
+ *   "email":"usr@gmail.com",
+ *   "email_verified":true,"iss":"https://*.auth0.com/",
+ *   "sub":"github|[0-9]",
+ *   "aud":"",
  *   "iat":1615322859,
  *   "exp":1615358859
  * }
